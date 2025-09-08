64c0cab7dd43e726c337aa3de6033cf81ad6ca2e
sharvgit <jsarvanisarvani@gmail.com>
2025-09-08 12:03:03 +0530

    Add files via upload

---
+++ day1_task.py
@@ -0,0 +1,60 @@
+
+
+#          ------------day_1 task --------------
+
+
+"""create a python script with both single-line and multi line comments explaing 
+the purpose of the script"""
+
+''' python is high level programming language, interactive and intrepeted programming
+language.python was deveoloped by " Guido van Russan ". It is a simple and readable.
+It is portable we can use in any device.
+
+'''
+
+""" Declare two variables ,one storing an integer another storing  an string element
+print their values  """
+
+# student_name="chakradhar"
+# student_no=198
+# print(student_name)
+# print(student_no)
+""" write a program  that prints a pattern using multiple print statements """
+
+
+# print("*******")
+# print("&&&&&&&&&&&")
+
+""" create a python script for a simple task add comments to explain each step"""
+# num_1=20# first value  20 
+# num_2=40  # second value 30
+# reslut=num_1+num_2 # later i added first value and second value
+# print(reslut)# inthis i got my reslut
+"""  create  a variables of differnt data types (int, float, str) and print their values 
+                          """
+# student_name="ram"
+# student_no=67
+# student_percentage=98.8
+# print(student_name)
+# print(student_no)
+# print(student_percentage)
+# print(type(student_name))
+# print(type(student_no))
+# print( type(student_percentage))
+
+"""  Determine the type of a variable 
+Expected output:
+the data type of variable x is < class 'int'>            """
+# x=100
+# print(type(x))
+
+"""  the memory address of x and y
+  x=10
+  y=10"""
+# x=10
+# y=10
+# print(id(x))
+# print(id(y))
+
+
+
